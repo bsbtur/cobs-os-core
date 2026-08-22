4f68943feea217777d1adca31fcb62aa0881d7e7
bsbtur <contato.bsbtur@gmail.com>
2026-08-21 21:57:54 -0300

    restrict participant action state rpc to authenticated users

---
+++ supabase/migrations/20260822005444_restrict_participant_action_state_rpc_to_authenticated.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.get_operation_participant_action_state(uuid, uuid) from public;
+grant execute on function public.get_operation_participant_action_state(uuid, uuid) to authenticated;
