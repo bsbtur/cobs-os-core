058ef223a9b22b289176d27c0b7b9df34ca2b7e5
bsbtur <contato.bsbtur@gmail.com>
2026-09-06 18:45:32 -0300

    docs: annotate CIOSP V3 registry semantics

---
+++ supabase/migrations/20260906213400_contracts_ciosp_2027_v3_comment.sql
@@ -0,0 +1,2 @@
+comment on column public.contract_templates.provider_template_id is
+  'Optional signature-provider integration metadata. It is not required to generate or legally activate a canonical COBS contract template.';
